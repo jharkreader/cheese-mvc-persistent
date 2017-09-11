c00bb3476face1385d0cdc26ead204680a4efa4c
Jennifer Harkreader <jen3072@gmail.com>
2017-09-10 19:06:38 -0700

    Added MenuController and add, index views

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,56 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("title", "Menus");
+        model.addAttribute("menus", menuDao.findAll());
+
+        return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addMenu(Model model) {
+
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+
+        return "menu/add";
+    }
+
+    @RequestMapping(value ="add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+
+        return "redirect:view/" + menu.getId();
+    }
+
+}
